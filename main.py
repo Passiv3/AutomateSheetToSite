44c38af18bee2c9cc52964703ad24fe815ccb76c
dc_surface <chungdanny56@gmail.com>
2023-10-29 00:23:07 -0700

    Progress, added functions to further facilitate reading spreadsheet

--- main.py
+++ main.py
@@ -5,34 +5,69 @@ import openpyxl.utils.exceptions as openpyxl_exceptions
 from selenium import webdriver
 from openpyxl import load_workbook
 
-driver = webdriver.Chrome()
+
+# driver = webdriver.Chrome()
 
 
 def entrypoint():
-    print("Enter the name of the mileage sheet. Don't forget the .xlsx extension: ")
-    file_name = input()
-    wb = read_file(file_name)
-    if wb is None:
-        print("read_file failed, exiting program...")
-    else:
-        pass
+    wb = read_file()
+    while wb is None:
+        print("read_file failed, try again...")
+        wb = read_file()
+    print_sheets(wb)
+    select_sheet(wb)
 
 
-def read_file(name):
+def get_number_input():
+    print("Which sheet did you want to read from?")
     try:
-        wb = load_workbook(filename=name)
-        print("File loaded successfully")
-        return wb
+        sheet_number = int(input())
+    except TypeError:
+        print("Incorrect type!")
+        get_number_input()
+    else:
+        return sheet_number
+
+
+def read_file():
+    """
+    Prints a prompt for user
+    Receives string input
+    Validates input
+    Returns
+    """
+    print("Enter the name of the mileage sheet. Don't forget the extension: ")
+    user_input_filename = input()
+    try:
+        workbook = load_workbook(filename=user_input_filename)
     except FileNotFoundError:
         print("File not found")
         return None
     except openpyxl_exceptions.InvalidFileException:
-        print("Incorrect Format")
+        print("Unsupported file format")
         return None
-    #finally:
-     #   return None
+    else:
+        print("File loaded successfully")
+        return workbook
 
-def parse_for_data(workbook, worksheet_number):
+
+def print_sheets(workbook):
+    print('Mileage sheets: ')
+    print(workbook.sheetnames)
+
+
+def select_sheet(workbook):
+    print("Which sheet would you like to use?")
+    user_input = input()
+    if user_input in workbook.sheetnames:
+        print("Copying "+ user_input)
+        current_worksheet = workbook[user_input]
+    else:
+        print(user_input + " sheet not found.")
+
+
+def parse_for_data(workbook, worksheet_number=0):
+
     pass
 
 
