61365079cff09ed600837e838e926730040eb280
dc_surface <chungdanny56@gmail.com>
2023-11-18 11:26:50 -0800

    Created function to get user info in selenium section, set options for chrome driver to prevent auto closing.

--- selenium_section.py
+++ selenium_section.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 mileage_url = "https://app.informedk12.com/link_campaigns/morgan-hill-travel-reimbursement-claim-electronic-form?token=fgtwSdEWCn8npWkHbYDbjfQH"
 
@@ -7,9 +8,23 @@ mileage_url = "https://app.informedk12.com/link_campaigns/morgan-hill-travel-rei
 #last 48829359
 
 
-def entrypoint():
-    driver = webdriver.Chrome()
+def entrypoint(list_of_rows=[]):
+    options = Options()
+    options.add_experimental_option("detach", True)
+    driver = webdriver.Chrome(options=options)
     driver.get(mileage_url)
 
+
+def get_user_info():
+    print("Enter your name: ")
+    name = input()
+    print("Enter your email: ")
+    email = input()
+    user_info = {name: name, email: email}
+    return user_info
+
+
+def locate_first_cell():
+    pass
 
 entrypoint()
